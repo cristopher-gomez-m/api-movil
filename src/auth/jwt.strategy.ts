41d94269c0c332e28eab5a9cb8d8af5293694588
cristopher-gomez-m <cristopher.gomezm@ug.edu.ec>
2023-06-05 09:27:02 -0500

    agregado el archivo vercel.json

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
-
+import {PassportStrategy} from "@nestjs/passport";
+import {Strategy} from 'passport-jwt';
 @Injectable()
-export class JwtStrategy{}
+export class JwtStrategy extends PassportStrategy(Strategy){}
